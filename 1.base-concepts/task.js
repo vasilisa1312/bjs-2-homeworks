d1638d5340f04fda130a92b30f687854e3997cf3
vasilisa1312 <Vasilisa.gorobchenko1994@mail.ru>
2023-06-14 22:19:21 +0300

    First task before tests

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,35 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
+
+  let arr;
+
+  let d = (b**2) - (4 * a * c);
+  if (d < 0) {
+    arr = []
+  }else if (d > 0) {
+    let d1=(-b+Math.sqrt(d))/(2*a);
+    let d2=(-b-Math.sqrt(d))/(2*a);
+    arr = [d1, d2]
+  }else if (d==0) {
+    let d3 = -b / ( 2 * a );
+    arr = [d3]
+  }
+
+  
   
   return arr;
 }
-Test
+
+
+
+
+
+
+
+
+
+
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
 }
