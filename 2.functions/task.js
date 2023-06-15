c3c0d9146b8ca368c9d34edd6c31e8d8f19933c3
vasilisa1312 <Vasilisa.gorobchenko1994@mail.ru>
2023-06-15 17:40:38 +0300

    First task redy

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,6 +1,21 @@
 function getArrayParams(...arr) {
+  let min = arr[0];
+  let max = arr[0];
+  let avg = 0;
 
-  return { min: min, max: max, avg: avg };
+  for(let i = 0; i < arr.length; i++) {
+    avg += arr[i];
+    if (arr[i] > max) {
+      max = arr[i];
+    }else if (arr[i] < min){
+      min = arr[i];
+    }
+  }
+
+  avg = avg / arr.length;
+  
+
+  return { min: min, max: max, avg: Number(avg.toFixed(2)) };
 }
 
 function summElementsWorker(...arr) {
