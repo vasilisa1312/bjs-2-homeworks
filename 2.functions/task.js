4c05d1a57c50e39d6fb3e27c2e39dd0db75b948d
vasilisa1312 <Vasilisa.gorobchenko1994@mail.ru>
2023-06-21 17:00:58 +0300

    ALL DONE

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -81,5 +81,18 @@ function averageEvenElementsWorker(...arr) {
 }
 
 function makeWork (arrOfArr, func) {
-
+  let maxWorkerResult = -Infinity;
+  
+  for (let i = 0; i < arrOfArr.length; i++) {
+    const semiResult = func(...arrOfArr[i]);
+    if (semiResult > maxWorkerResult){
+      maxWorkerResult = semiResult;
+    }
+  }
+  return maxWorkerResult;
 }
+
+const arr = [[10, 10, 11, 20, 10], [67, 10, 2, 39, 88], [72, 75, 51, 87, 43], [30, 41, 55, 96, 62]];
+let newArr = [...arr]
+console.log(newArr)
+console.log(arr)
