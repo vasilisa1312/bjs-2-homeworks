f5d3f017d7ce3152618561306ee69125c33e1d0e
vasilisa1312 <Vasilisa.gorobchenko1994@mail.ru>
2023-06-26 23:32:26 +0300

    OMW

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,51 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
+new Student("Игорь", "Male", 21)
+new Student("Полина", "Female", 20)
+new Student("Оксана", "Female", 23)
+
+
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if(this.marks !==undefined){
+    this.marks.push(...marks);
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+    let sum = 0;
+    for(let i = 0; i < this.marks.length; i++) {
+    sum += this.marks[i];
+  }
+  let result = sum/this.marks.length;
+  if (isNaN(result)) {
+    return 0;
+  }else {
+  return result;
+  }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
+
+
+let student1 = new Student("Василиса", "женский", 19);
+student1.setSubject("Algebra");
+console.log(student1.getAverage());
+student1.addMarks(4, 5, 4, 5);
+console.log(student1.getAverage())
+let student2 = new Student("Артём", "мужской", 25);
+student2.setSubject("Geometry");
+student2.exclude('плохая учёба')
+console.log(student2)
