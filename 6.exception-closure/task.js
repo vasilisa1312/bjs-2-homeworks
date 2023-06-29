2236fad873646878593e626578ea2d5ba5f20f05
vasilisa1312 <Vasilisa.gorobchenko1994@mail.ru>
2023-06-29 19:39:56 +0300

    second task on working

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -22,7 +22,7 @@ class Triangle {
         this.sideB = sideB;
         this.sideC = sideC;
         
-        if(sideA + sideB < sideB){
+        if(sideA + sideB < sideC || sideA + sideC < sideB || sideB + sideC < sideA ){
             throw new Error("Треугольник с такими сторонами не существует");
         }
         
@@ -35,12 +35,25 @@ class Triangle {
         let p = this.perimetr * 0.5;
         let s = Math.sqrt(p * (p - this.sideA) * (p - this.sideB) * (p - this.sideC));
         let result = s.toFixed(3);
+        result = Number(result);
         return result;
     }
 
 }
 
 
-function getTriangle(a,b,c){
-    return new Triangle;
+function getTriangle(sideA, sideB, sideC){
+   try{
+    return new Triangle(sideA, sideB, sideC);
+   } catch (error){
+        return obj = {
+        get perimetr() {
+                return "Ошибка! Треугольник не существует";
+            },
+        get area(){
+                return "Ошибка! Треугольник не существует";
+            }
+        }
+    
+   }
 }
