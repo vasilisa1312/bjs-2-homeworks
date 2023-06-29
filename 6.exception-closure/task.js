782c1570c1685fef35ebfa5d66b7fdd03d5e80d3
vasilisa1312 <Vasilisa.gorobchenko1994@mail.ru>
2023-06-29 16:18:59 +0300

    Second task on JOB

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,46 @@
-﻿
+﻿function parseCount(arg) {
+    let result = Number.parseFloat(arg);
+    if(isNaN(result)) {
+        throw new Error("Невалидное значение");
+    }
+    return result;
+}
+
+function validateCount(arg){
+    try{
+        return parseCount(arg);
+    } catch(error){
+        return error;
+    }
+        
+}
+
+
+class Triangle {
+    constructor(sideA, sideB, sideC){
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+        
+        if(sideA + sideB < sideB){
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+        
+    }
+    get perimetr(){
+        return this.sideA + this.sideB + this.sideC;
+    }
+    
+    get area(){
+        let p = this.perimetr * 0.5;
+        let s = Math.sqrt(p * (p - this.sideA) * (p - this.sideB) * (p - this.sideC));
+        let result = s.toFixed(3);
+        return result;
+    }
+
+}
+
+
+function getTriangle(a,b,c){
+    return new Triangle;
+}
