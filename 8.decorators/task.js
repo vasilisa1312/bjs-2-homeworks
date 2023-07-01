6b3b86e4df40ba20394c8b8b5ba86347b2f3edf2
vasilisa1312 <Vasilisa.gorobchenko1994@mail.ru>
2023-07-01 17:16:27 +0300

    ALL TASK is done without comments

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -3,17 +3,17 @@ function cachingDecoratorNew(func) {
     let cache = [];
 
     function wrapper(...args) {
-        const hash = md5(args) // получаем правильный хеш c помощью функции md5
-        let objectInCache = cache.find((item) => item.hash === hash); // ищем элемент, хеш которого равен нашему хешу
-        if (objectInCache) { // если элемент найден
-            console.log("Из кэша: " + objectInCache.value); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+        const hash = md5(args) 
+        let objectInCache = cache.find((item) => item.hash === hash); 
+        if (objectInCache) { 
+            console.log("Из кэша: " + objectInCache.value); 
             return "Из кэша: " + objectInCache.value;
         }
     
-        let result = func(...args); // в кеше результата нет — придётся считать
-        cache.push({hash, value: result}) ; // добавляем элемент с правильной структурой
+        let result = func(...args); 
+        cache.push({hash, value: result}) ; 
         if (cache.length > 5) { 
-          cache.shift(); // если слишком много элементов в кеше, надо удалить самый старый (первый) 
+          cache.shift(); 
         }
         console.log("Вычисляем: " + result);
         return "Вычисляем: " + result;  
